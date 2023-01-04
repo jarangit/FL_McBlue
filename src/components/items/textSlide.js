c0f049dc022a877f262a440df05b32b47e9e201d
jarangit <jaran.dch@gmail.com>
2023-01-04 23:09:48 +0700

    fix: text slide

--- src/components/items/textSlide.js
+++ src/components/items/textSlide.js
@@ -3,9 +3,9 @@ import { HiVolumeUp } from 'react-icons/hi'
 
 const TextSlide = () => {
   return (
-    <div className={` pl-3 md:pl-[200px] absolute top-[75px] w-full my_bg_blue overflow-hidden py-1`}>
-      <div className='flex items-center '>
-        <div className={` z-10 my_bg_blue absolute h-full  flex items-center p-1`}>
+    <div className={` absolute top-[75px] w-full  my_bg_blue overflow-hidden py-1`}>
+      <div className='flex items-center relative md:ml-[200px]  overflow-hidden'>
+        <div className={` z-10 my_bg_blue absolute h-full  flex items-center p-1  -left-0`}>
           <HiVolumeUp size={20} />
         </div>
         <div className='animate-marquee2 whitespace-nowrap w-full font-bold'>
